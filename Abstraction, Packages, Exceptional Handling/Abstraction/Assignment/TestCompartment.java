0f0358b952b21f76c4609afafaaea43f831d2e6f
krisanjay16 <70562888+krisanjay16@users.noreply.github.com>
2022-02-12 08:24:55 +0530

    Create TestCompartment.java

---
+++ Abstraction, Packages, Exceptional Handling/Abstraction/Assignment/TestCompartment.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+
+abstract class Compartment{
+	public abstract String notice();
+}
+
+class FirstClass extends Compartment{
+	public String notice() {
+		return "You are in the First Class Compartment";
+	}
+}
+
+class Ladies extends Compartment{
+	public String notice() {
+		return "You are in the Ladies Compartment";
+	}
+}
+
+class General extends Compartment{
+	public String notice() {
+		return "You are in the General Compartment";
+	}
+}
+
+class Luggage extends Compartment{
+	public String notice() {
+		return "You are in the Luggage Compartment";
+	}
+}
+
+public class TestCompartment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Compartment[] compartments = new Compartment[10];
+
+		Random random = new Random();
+		
+	    	for (int i = 0; i < 10; i++) {
+	    		int randomNum = random.nextInt((4 - 1) + 1) + 1;
+	    	
+	    		if (randomNum == 1)
+	    			compartments[i] = new Luggage();
+	    		else if (randomNum == 2)
+	    			compartments[i] = new Ladies();
+	    		else if (randomNum == 3)
+	    			compartments[i] = new General();
+	    		else if (randomNum == 4)
+	    			compartments[i] = new FirstClass();
+	    	
+	    		System.out.println(compartments[i].notice());
+	    	}
+
+	}
+
+}
